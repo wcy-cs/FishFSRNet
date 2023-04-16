19d07d6944ed526426a0c4030f3eeea61b70f8b7
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2023-04-16 10:48:56 +0800

    Update test_parsingnet.py

--- parsing/test_parsingnet.py
+++ parsing/test_parsingnet.py
@@ -2,7 +2,7 @@ from option import args
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = args.cuda_name
 import torch
-import data_parsingnet
+import dataset_parsingnet
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import os
@@ -13,7 +13,7 @@ net = parsingnet.ParsingNet()
 net = util.prepare(net)
 print(util.get_parameter_number(net))
 writer = SummaryWriter('./logs/{}'.format(args.writer_name))
-testdata = data_parsingnet.Data(root=os.path.join(args.dir_data, args.data_test), args=args, train=False)
+testdata = dataset_parsingnet.Data(root=os.path.join(args.dir_data, args.data_test), args=args, train=False)
 testset = DataLoader(testdata, batch_size=1, shuffle=False, num_workers=1)
 pretrained_dict = torch.load('./epoch.pth', map_location='cuda:0')
 net.load_state_dict(pretrained_dict)
