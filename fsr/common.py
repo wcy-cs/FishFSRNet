e8fbfa4e7e690a19ddd76f979843cb20bf3dfe96
wcy-cs <39185517+wcy-cs@users.noreply.github.com>
2024-01-22 19:42:54 +0800

    Update common.py

--- fsr/common.py
+++ fsr/common.py
@@ -67,36 +67,27 @@ class ResBlock(nn.Module):
         return res
 
 
+
+
 class Upsampler(nn.Sequential):
-    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+    def __init__(self, scale, n_feats):
+        super().__init__()
 
         m = []
-        if (scale & (scale - 1)) == 0:  # Is scale = 2^n?
+        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
             for _ in range(int(math.log(scale, 2))):
-                m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(nn.Conv2d(n_feats, 4 * n_feats, 3, 1, 1))
                 m.append(nn.PixelShuffle(2))
-                if bn:
-                    m.append(nn.BatchNorm2d(n_feats))
-                if act == 'relu':
-                    m.append(nn.ReLU(True))
-                elif act == 'prelu':
-                    m.append(nn.PReLU(n_feats))
 
         elif scale == 3:
-            m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(nn.Conv2d(n_feats, 9 * n_feats, 3, 1, 1))
             m.append(nn.PixelShuffle(3))
-            if bn:
-                m.append(nn.BatchNorm2d(n_feats))
-            if act == 'relu':
-                m.append(nn.ReLU(True))
-            elif act == 'prelu':
-                m.append(nn.PReLU(n_feats))
         else:
             raise NotImplementedError
-
-        super(Upsampler, self).__init__(*m)
-
-
+        self.body = nn.Sequential(*m)
+    def forward(self, x):
+        res = self.body(x)
+        return res
 
 
 
